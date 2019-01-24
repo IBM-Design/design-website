5544f64561d563ac088a109fe408cffd6d8cb1fe
Josefina Mancilla <josefinanoemi4@gmail.com>
2019-01-24 12:42:45 -0600

    fix(carousel): add unique id for multi carousel func.

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -5,15 +5,25 @@ import { RadioButtonGroup, RadioButton } from 'carbon-components-react';
 import { ChevronLeft32, ChevronRight32 } from '@carbon/icons-react';
 
 export default class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onChange = this.onChange.bind(this);
+  }
+
   static propTypes = {
     /**
      * for slide images
      */
     children: PropTypes.node,
+    /**
+     * unique id for each carousel, required
+     */
+    id: PropTypes.string,
   };
 
   onChange = e => {
-    const slide = document.querySelector('.ibm--carousel-slide');
+    const slide = document.querySelector(`.${this.props.id}`);
     const images = slide.querySelectorAll('img');
 
     images.forEach(img => {
@@ -22,7 +32,7 @@ export default class Carousel extends React.Component {
   };
 
   render() {
-    const { children } = this.props;
+    const { children, id } = this.props;
     const img = children[1].props.children[3].props;
     const img2 = children[3].props.children[3].props;
     const img3 = children[5].props.children[3].props;
@@ -31,7 +41,7 @@ export default class Carousel extends React.Component {
     return (
       <div className="ibm--carousel">
         <div className="ibm--carousel-slide-wrapper">
-          <div className="ibm--carousel-slide">
+          <div className={`ibm--carousel-slide ${this.props.id}`}>
             <img src={img.src} alt={img.alt} className={img.className} />
             <img src={img2.src} alt={img2.alt} className={img2.className} />
             <img src={img3.src} alt={img3.alt} className={img3.className} />
@@ -40,9 +50,9 @@ export default class Carousel extends React.Component {
         </div>
         <RadioButtonGroup
           className="ibm--carousel-nav-wrapper"
-          name="Carousel nav"
+          name={`Carousel nav ${this.props.id}`}
           valueSelected={1}
-          onChange={e => this.onChange(e)}>
+          onChange={this.onChange}>
           <RadioButton
             className="ibm--carousel-nav-item"
             value={1}
