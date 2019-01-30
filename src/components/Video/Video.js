8c1fcb17347daeed31e68e6bc4587cb970544bf8
Josefina Mancilla <josefinanoemi4@gmail.com>
2019-01-30 10:27:04 -0600

    refactor(video): add play icon

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -42,23 +42,106 @@ export default class Video extends React.Component {
   };
 
   onEnd = () => {
+    console.log('VIDEO ENDED');
     const video = document.querySelector('.ibm--video-wrapper');
-    video.classList.remove('active');
-    const iframe = video.querySelector('iframe');
+    if (video.classList.contains('active')) {
+      video.classList.remove('active');
+    }
+    const iframe = document.querySelector('iframe');
     const player = new Player(iframe);
     player.setCurrentTime(0);
     player.setVolume(0);
-    player.loadVideo(304672438);
+    player.play();
   };
 
   render() {
     const { children } = this.props;
+    const svgPlayBtn = (
+      <svg
+        className="ibm--video-play-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 91 91">
+        <defs>
+          <clipPath id="clip-path" transform="translate(-610 -1009)">
+            <circle class="cls-1" cx="655.5" cy="1054.5" r="36" />
+          </clipPath>
+          <clipPath id="clip-path-2" transform="translate(-610 -1009)">
+            <rect class="cls-1" width="1312" height="2638" />
+          </clipPath>
+          <clipPath id="clip-path-3" transform="translate(-610 -1009)">
+            <rect class="cls-1" x="615" y="1014" width="81" height="81" />
+          </clipPath>
+          <clipPath id="clip-path-5" transform="translate(-610 -1009)">
+            <rect class="cls-1" x="619" y="1018" width="73" height="73" />
+          </clipPath>
+          <clipPath id="clip-path-6" transform="translate(-610 -1009)">
+            <path
+              class="cls-2"
+              d="M648.5,1042.63a1,1,0,0,0-.5.87v22a1,1,0,0,0,1,1,1,1,0,0,0,.5-.14l19-11a1,1,0,0,0,0-1.73l-19-11a1,1,0,0,0-.5-.13A1,1,0,0,0,648.5,1042.63Z"
+            />
+          </clipPath>
+          <clipPath id="clip-path-8" transform="translate(-610 -1009)">
+            <rect class="cls-1" x="642" y="1037" width="32" height="35" />
+          </clipPath>
+          <clipPath id="clip-path-9" transform="translate(-610 -1009)">
+            <rect class="cls-1" x="643" y="1037.5" width="31" height="34" />
+          </clipPath>
+          <clipPath id="clip-path-10" transform="translate(-610 -1009)">
+            <rect class="cls-1" x="647" y="1042" width="22" height="25" />
+          </clipPath>
+          <clipPath id="clip-path-11" transform="translate(-610 -1009)">
+            <rect class="cls-1" x="642" y="1038.5" width="32" height="32" />
+          </clipPath>
+        </defs>
+        <g id="Layer_2" data-name="Layer 2">
+          <g id="Layer_1-2" data-name="Layer 1">
+            <g class="cls-3">
+              <g class="cls-4">
+                <g class="cls-5">
+                  <g class="cls-6">
+                    <g class="cls-6">
+                      <g class="cls-7">
+                        <rect class="cls-8" width="91" height="91" />
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </g>
+            <g class="cls-9">
+              <g class="cls-4">
+                <g class="cls-5">
+                  <g class="cls-10">
+                    <g class="cls-11">
+                      <g class="cls-12">
+                        <rect x="28" y="23.5" width="41" height="44" />
+                      </g>
+                    </g>
+                    <g class="cls-13">
+                      <g class="cls-12">
+                        <rect
+                          class="cls-14"
+                          x="27"
+                          y="24.5"
+                          width="42"
+                          height="42"
+                        />
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+      </svg>
+    );
 
     return (
       <div className="ibm--video-wrapper" onClick={this.onClick}>
         {children}
         <div className="ibm--video-overlay" />
-        <div className="ibm--video-controls" />
+        <div className="ibm--video-controls">{svgPlayBtn}</div>
       </div>
     );
   }
