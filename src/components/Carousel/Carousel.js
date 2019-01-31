1b877596915a260c38dad594864726f31e59787d
Josefina Mancilla <josefinanoemi4@gmail.com>
2019-01-31 09:47:07 -0600

    feat(carousel): add click/drag event

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
 import { RadioButtonGroup, RadioButton } from 'carbon-components-react';
 import { ChevronLeft32, ChevronRight32 } from '@carbon/icons-react';
 
@@ -17,8 +16,10 @@ export default class Carousel extends React.Component {
 
   componentDidMount() {
     const slide = document.querySelector(`.${this.props.id}`);
-    slide.addEventListener('touchstart', this.handleStart, false);
-    slide.addEventListener('touchmove', this.moveTouch, false);
+    slide.addEventListener('touchstart', this.touchStart, false);
+    slide.addEventListener('touchmove', this.touchMove, false);
+    slide.addEventListener('mousedown', this.mouseStart);
+    slide.addEventListener('mousemove', this.mouseMove);
     const autoplay = setInterval(this.nextSlide, 6000);
   }
 
@@ -36,12 +37,13 @@ export default class Carousel extends React.Component {
   initialX = null;
   initialY = null;
 
-  handleStart = e => {
+  //TOUCH EVENT HANDLERS
+  touchStart = e => {
     this.initialX = e.touches[0].clientX;
     this.initialY = e.touches[0].clientY;
   };
 
-  moveTouch = e => {
+  touchMove = e => {
     if (this.initialX === null) {
       return;
     }
@@ -87,6 +89,59 @@ export default class Carousel extends React.Component {
     e.preventDefault();
   };
 
+  //MOUSE EVENT HANDLERS
+  mouseStart = e => {
+    this.initialX = e.clientX;
+    this.initialY = e.clientY;
+    console.log('initialX,Y:', this.initialX, this.initialY);
+  };
+
+  mouseMove = e => {
+    if (this.initialX === null) {
+      return;
+    }
+
+    if (this.initialY === null) {
+      return;
+    }
+    const finalX = e.clientX;
+    const finalY = e.clientY;
+    const diffX = this.initialX - finalX;
+    const diffY = this.initialY - finalY;
+    const state = this.state.checkedRadio;
+    console.log('final X,Y:', finalX, finalY);
+
+    if (Math.abs(diffX) > Math.abs(diffY)) {
+      if (diffX > 0) {
+        // swiped left
+        if (state === 1) {
+          this.onChange(2);
+        } else if (state === 2) {
+          this.onChange(3);
+        } else if (state === 3) {
+          this.onChange(4);
+        } else if (state === 4) {
+          this.onChange(1);
+        }
+      } else {
+        // swiped right
+        if (state === 1) {
+          this.onChange(4);
+        } else if (state === 2) {
+          this.onChange(1);
+        } else if (state === 3) {
+          this.onChange(2);
+        } else if (state === 4) {
+          this.onChange(3);
+        }
+      }
+    }
+    this.initialX = null;
+    this.initialY = null;
+    e.preventDefault();
+  };
+
+  //AUTOPLAY FUNC.
   nextSlide = () => {
     const state = this.state.checkedRadio;
     if (state === 1) {
@@ -100,6 +155,7 @@ export default class Carousel extends React.Component {
     }
   };
 
+  //UPDATING RADIO BUTTON
   onChange = e => {
     const slide = document.querySelector(`.${this.props.id}`);
     const images = slide.querySelectorAll('img');
@@ -120,10 +176,30 @@ export default class Carousel extends React.Component {
       <div className={`ibm--carousel ${this.props.id}`}>
         <div className="ibm--carousel-slide-wrapper">
           <div className={`ibm--carousel-slide ${this.props.id}`}>
-            <img src={img.src} alt={img.alt} className={img.className} />
-            <img src={img2.src} alt={img2.alt} className={img2.className} />
-            <img src={img3.src} alt={img3.alt} className={img3.className} />
-            <img src={img4.src} alt={img4.alt} className={img4.className} />
+            <img
+              draggable="false"
+              src={img.src}
+              alt={img.alt}
+              className={img.className}
+            />
+            <img
+              draggable="false"
+              src={img2.src}
+              alt={img2.alt}
+              className={img2.className}
+            />
+            <img
+              draggable="false"
+              src={img3.src}
+              alt={img3.alt}
+              className={img3.className}
+            />
+            <img
+              draggable="false"
+              src={img4.src}
+              alt={img4.alt}
+              className={img4.className}
+            />
           </div>
         </div>
         <RadioButtonGroup
